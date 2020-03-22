9bca59b07e0eec5d9f41be3991b45dc20c54f750
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2020-03-22 18:43:08 +0100

    tunning hyperparameters 1

--- prueba.py
+++ prueba.py
@@ -112,7 +112,7 @@ import xgboost as xgb
 from xgboost.sklearn import XGBClassifier
 from sklearn.metrics import accuracy_score
 
-
+'''
 #X_train, y_train = IPF(X_train, y_train)
 #print("Numero de instancias: " + str(len(X_train)))
 #print("Instancias por clase:")
@@ -135,3 +135,15 @@ xgb1 = XGBClassifier(
 model = xgb1.fit(X_train,y_train)
 pred = model.predict(X_test)
 print(accuracy_score(pred,y_test))
+'''
+## Tune max_depth and min_child_weight
+param_test1 = {
+ 'max_depth':range(3,10,2),
+ 'min_child_weight':range(1,6,2)
+}
+gsearch1 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=140, max_depth=5,
+ min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1, seed=27),
+ param_grid = param_test1, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch1.fit(X_train,y_train)
+print(gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_)
