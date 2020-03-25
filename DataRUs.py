56b6a98a422ac3400438f34dfad86ab9948cc486
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2020-03-25 02:08:19 +0100

    ole

--- DataRUs.py
+++ DataRUs.py
@@ -11,6 +11,7 @@ import pandas as pd
 ################################################################################
 # PLOTS BASADOS EN TSNE
 def plotData(X, y, route):
+    X.drop(['CADASTRALQUALITYID'],axis=1)
     X = np.array(X)
     reduced = TSNE(n_components=2, n_jobs=-1).fit_transform(X)
 
